4fe9d9ad08eb2e81e327d53ad31fa51fd9cb2267
Radina Valentinova Dimitrova <Radina.Dimitrova@allocatesoftware.com>
2018-07-25 14:19:45 +0300

    Lecture 1 examples with Java

---
+++ src/Zadacha4.java
@@ -0,0 +1,16 @@
+public class Zadacha4 {
+
+    //Въведете 2 различни числа от конзолата и ги разпечатайте в нарастващ ред.
+
+    public static void main(String[] args) {
+        int x = 20;
+        int y = 30;
+        if (x < y) {
+            System.out.println("Ascending order:");
+            System.out.println(x);
+            System.out.println(y);
+        }
+    }
+
+}
+
